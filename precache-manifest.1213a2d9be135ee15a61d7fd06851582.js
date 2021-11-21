132389ddf5afa91c47b7eaa29727ff26031fd8af
Nicholas Čapek <nicholas.capek@generaliceska.cz>
2021-11-21 21:11:37 +0100

    init

---
+++ precache-manifest.1213a2d9be135ee15a61d7fd06851582.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2151c4e3c2f936dba208149302836f94",
+    "url": "/ai-overview/index.html"
+  },
+  {
+    "revision": "444598d83edfa3e1238e",
+    "url": "/ai-overview/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "3dff7ac9395c40f2dc1e",
+    "url": "/ai-overview/static/css/main.58f35653.chunk.css"
+  },
+  {
+    "revision": "444598d83edfa3e1238e",
+    "url": "/ai-overview/static/js/2.d265c860.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/ai-overview/static/js/2.d265c860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "231fabbb727fcf5e5c03",
+    "url": "/ai-overview/static/js/3.04f5861a.chunk.js"
+  },
+  {
+    "revision": "3dff7ac9395c40f2dc1e",
+    "url": "/ai-overview/static/js/main.d958910a.chunk.js"
+  },
+  {
+    "revision": "f26db274242307752078",
+    "url": "/ai-overview/static/js/runtime-main.910f445f.js"
+  },
+  {
+    "revision": "0f46ed8c97496ccf47a85d18f0b331ca",
+    "url": "/ai-overview/static/media/ai.0f46ed8c.jpg"
+  },
+  {
+    "revision": "3843f24c320d44adecc5de335d65f613",
+    "url": "/ai-overview/static/media/regrese.3843f24c.png"
+  }
+]);
